9492043173e40ec933257472a5ee458c74652b42
yunuserbek <yunus_erbek@windowslive.com>
2022-08-28 16:04:14 +0300

    SmoothBottomBar

--- app/src/main/java/com/example/cryptoappapi/MainActivity.kt
+++ app/src/main/java/com/example/cryptoappapi/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.cryptoappapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.cryptoappapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navhostFragment =supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navhostFragment!!.findNavController()
+
+        val popupMenu =PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            title = when (destination.id) {
+                R.id.watchlistFragment -> ""
+                R.id.marketFragment2 -> ""
+                else -> ""
+            }
+        }
     }
 }
